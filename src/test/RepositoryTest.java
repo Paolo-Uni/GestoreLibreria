65d81c152fafe6f1a3fac39393341229bfae2e44
paolo <paolosicoli07@gmail.com>
2025-06-13 20:01:13 +0200

    Classi implementate (package command, filtro): Command(interfaccia), ModificaCommand,Filtro(interfaccia), FiltroPerGenere, FiltroPerStato,FiltroPerValutazioneMinima.
    Classi aggiornate: GestoreLibreria, RepositoryTest
    
    Funzioni implementate: operazioni legate al filtraggio dei libri + è stato rimosso il metodo modifica da GestoreLibreria (se ne occuperà Command)
    
    Aggiunti i test relativi a: Filtro, GestoreLibreria, Command

--- src/test/RepositoryTest.java
+++ src/test/RepositoryTest.java
@@ -7,6 +7,7 @@ import progetto.Libro;
 import progetto.repository.JsonRepository;
 import progetto.repository.Repository;
 import progetto.repository.RepositoryFactory;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,17 +24,17 @@ public class RepositoryTest {
 
     @Test
     void testSalvaECaricaListaLibri() {
-        Libro libro = new Libro("Test", "1", "99999999", "Romanzo", 4, Libro.StatoLettura.LETTO);;
+        Libro libro = new Libro("Test", "Autore", "123", "Genere", 4, Libro.StatoLettura.IN_LETTURA);
         repository.salva(List.of(libro));
 
         List<Libro> caricati = repository.carica();
         assertEquals(1, caricati.size());
-        assertEquals("99999999", caricati.getFirst().getIsbn());
+        assertEquals("123", caricati.getFirst().getIsbn());
     }
 
     @Test
     void testSalvaSingoloLibroNonDuplicato() {
-        Libro libro = new Libro("Test", "2", "88888888", "Romanzo", 4, Libro.StatoLettura.LETTO);
+        Libro libro = new Libro("Titolo", "Autore", "ABC", "Genere", 3, Libro.StatoLettura.DA_LEGGERE);
         assertTrue(gestore.aggiungiLibro(libro));
         assertFalse(gestore.aggiungiLibro(libro)); // Tentativo di salvataggio duplicato
     }
@@ -43,5 +44,6 @@ public class RepositoryTest {
         Repository repo = RepositoryFactory.creaRepository("json");
         assertNotNull(repo);
         assertInstanceOf(JsonRepository.class, repo);
+        gestore.eliminaLibreria();
     }
-} 
+}
